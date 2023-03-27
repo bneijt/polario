aa114dfbe0e117fb3c0ec0c080ee6ca3244cf5d8
Bram Neijt <bram@neijt.nl>
2023-03-27 16:39:02 +0200

    Add command structure

--- polario/main.py
+++ polario/main.py
@@ -1,12 +1,20 @@
 """Main CLI interface
 """
 import argparse
+import json
+import sys
+from enum import Enum
 from pathlib import Path
-from pprint import pprint
 
 import polars as pl
 
 from polario import __version__
+
+
+class Command(Enum):
+    SHOW = "show"
+    SCHEMA = "schema"
+    JSON_HEAD = "json_head"
 
 
 def main() -> int:
@@ -20,26 +28,29 @@ def main() -> int:
         version="%(prog)s {version}".format(version=__version__),
     )
     parser.add_argument(
+        "cmd",
+        choices=[c.value for c in Command],
+        help="command to run",
+    )
+    parser.add_argument(
         "paths", metavar="PATH", type=Path, nargs="+", help="input paths"
     )
     args = parser.parse_args()
-
+    cmd = Command(args.cmd)
     for path in args.paths:
         if path.is_dir():
             raise ValueError(
                 "Input path must be a file. File an issue if you want dataset support."
             )
         df = pl.read_parquet(path, use_pyarrow=True)
-        print(df)
-
-        if len(df):
-            print("First row:")
-            pprint(df.limit(1).to_dicts()[0])
-        # print(type(path))
+        if cmd == Command.SHOW:
+            print(df)
+        elif cmd == Command.SCHEMA:
+            print(df.schema())
+        elif cmd == Command.JSON_HEAD:
+            json.dump(df.head().to_dicts(), sys.stdout, indent=2)
     return 0
 
 
 if __name__ == "__main__":
-    import sys
-
     sys.exit(main())
